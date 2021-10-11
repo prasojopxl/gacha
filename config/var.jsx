e7b379d1d501b128e866580965eb1680b8486120
prasojopxl <prasojodesigner@gmail.com>
2021-10-11 12:38:18 +0700

    f

--- config/var.jsx
+++ config/var.jsx
@@ -1,3 +1,2 @@
 export const baseUrl = `https://gacha-sigma.vercel.app/`;
 export const apiUrl = `https://abiyyu-dev.com/allApi/rest_ci/index.php/`;
-export const apiTest = `https://jsonplaceholder.typicode.com/`
