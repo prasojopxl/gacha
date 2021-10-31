02345feea622563267192054236a262e7db9e119
prasojopxl <prasojodesigner@gmail.com>
2021-10-31 09:27:48 +0700

    change baseurl

--- config/var.jsx
+++ config/var.jsx
@@ -1,4 +1,4 @@
-export const baseUrl = `https://gacha-nine.vercel.app/`;
+export const baseUrl = `https://gacha.onenetwork.id/`;
 export const apiUrl = `https://abiyyu-dev.com/allApi/rest_ci/index.php/`;
 export const apiUrl2 = `https://admin07.onenetwork.id/api/`;
 export const imageUrl = `https://admin07.onenetwork.id/assets/uploads/hadiah/`
